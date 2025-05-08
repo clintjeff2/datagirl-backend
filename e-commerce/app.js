62c1e13d722e973046584ba829c46f9727d0db0f
clintjeff2 <clintjeff2@gmail.com>
2025-05-08 11:52:08 +0100

    Defined routes for accessing controller functions

--- e-commerce/app.js
+++ e-commerce/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const { createUser } = require('./controllers/user.controller');
+const {
+	createUser,
+	getUser,
+	getAllUsers,
+	updateUser,
+} = require('./controllers/user.controller');
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.post('/user', createUser)
+app.post('/user', createUser);
+app.get('/user', getAllUsers);
+app.get('/user/:id', getUser);
+app.patch('/user/:id', updateUser);
 
 module.exports = app;
-
