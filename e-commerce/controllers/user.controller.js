0249317dd273122d327c772c6acd13b72f6df05a
clintjeff2 <clintjeff2@gmail.com>
2025-05-08 11:51:45 +0100

    Created createUser, getUser, getAllUsers, and updateUser controllers

--- e-commerce/controllers/user.controller.js
+++ e-commerce/controllers/user.controller.js
@@ -5,12 +5,50 @@ exports.createUser = async (req, res) => {
 		// res.write('Received Request to Create User');
 		const data = req.body;
 
-		console.log(data);
 		const user = await User.create(data);
+		// const user = new User(data)
+		// user.save();
 
-		res.status(200).json({ message: 'Successs in creating user' });
+		res.status(201).json({ message: 'Successs in creating user' });
 	} catch (err) {
 		console.log(err);
 		res.status(400).json({ message: 'Failed', error: err });
+	}
+};
+
+exports.getUser = async (req, res) => {
+	try {
+		const userId = req.params.id;
+
+		const user = await User.find({ _id: userId });
+		// const user = await User.findById(userId);
+
+		res.status(200).json({ message: 'Success', user });
+	} catch (err) {
+		res.status(500).json({ message: 'Failed', err });
+	}
+};
+
+exports.getAllUsers = async (req, res) => {
+	try {
+		const users = await User.find();
+
+		res.status(200).json({ message: 'success', results: users.length, users });
+	} catch (err) {
+		res.status(500).json({ message: 'Failed', err });
+	}
+};
+
+exports.updateUser = async (req, res) => {
+	try {
+		const userId = req.params.id;
+		const data = req.body;
+
+		const user = await User.findByIdAndUpdate(userId, data);
+		// const user = await User.findOneAndUpdate({_id: userId}, data)
+
+		res.status(200).json({ message: 'success', user });
+	} catch (err) {
+		res.status(500).json({ message: 'Failed', err });
 	}
 };
