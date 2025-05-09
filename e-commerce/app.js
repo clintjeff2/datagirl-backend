98c6caeecc125a4594ac2672be786c8eccd62be9
clintjeff2 <clintjeff2@gmail.com>
2025-05-09 18:20:38 +0100

    Added routes for the store model in main

--- e-commerce/app.js
+++ e-commerce/app.js
@@ -5,6 +5,13 @@ const {
 	getAllUsers,
 	updateUser,
 } = require('./controllers/user.controller');
+const {
+	deleteStore,
+	createStore,
+	getStore,
+	getAllStores,
+	updateStore,
+} = require('./controllers/store.controller');
 
 const app = express();
 
@@ -14,5 +21,11 @@ app.post('/user', createUser);
 app.get('/user', getAllUsers);
 app.get('/user/:id', getUser);
 app.patch('/user/:id', updateUser);
+
+app.post('/store', createStore);
+app.get('/store', getAllStores);
+app.get('/store/:id', getStore);
+app.patch('/store/:id', updateStore);
+app.delete('/store/:id', deleteStore);
 
 module.exports = app;
