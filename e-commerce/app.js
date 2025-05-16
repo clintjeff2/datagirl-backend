cb2eb9290ce932d15fdf154f6364dd1976cf3b92
clintjeff2 <clintjeff2@gmail.com>
2025-05-16 12:08:44 +0100

    protected the category route and showed examples of how to use categories

--- e-commerce/app.js
+++ e-commerce/app.js
@@ -18,10 +18,27 @@ const {
 	getCategory,
 } = require('./controllers/category.controller');
 
-const { login, protect } = require('./controllers/auth.controller');
+const {
+	login,
+	protect,
+	test1,
+	restrictTo,
+} = require('./controllers/auth.controller');
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+	console.log('Our first middleware for today');
+
+	next();
+});
+
+app.use((req, res, next) => {
+	console.log('Our SECOND middleware for today');
+
+	next();
+});
 
 app.post('/user', createUser);
 app.get('/user', getAllUsers);
@@ -34,11 +51,11 @@ app.get('/store/:id', getStore);
 app.patch('/store/:id', updateStore);
 app.delete('/store/:id', deleteStore);
 
-app.post('/category', createCategory);
+app.post('/category', protect, restrictTo(['seller', 'admin']), createCategory);
 app.get('/category', getAllCategories);
 app.get('/category/:id', getCategory);
 
 app.get('/login', login);
-app.post('/protect', protect);
+app.post('/protect', test1, protect);
 
 module.exports = app;
